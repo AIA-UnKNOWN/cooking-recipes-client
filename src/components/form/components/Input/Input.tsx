af13e386c020601277c5d1ae64c8df2d06c92a50
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-09-17 23:28:39 +0800

    added components path for global use of components like forms or its elements

---
+++ src/components/form/components/Input/Input.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Input = props => {
+  return (
+    <input
+      {...props}
+      className={`
+        h-[40px] bg-[#E9E9E9] text-[15px] px-4 rounded-sm
+        w-full focus:outline focus:outline-[#DAC3FF] focus:outline-[3px]
+      `}
+    />
+  );
+}
+
+export default Input;
