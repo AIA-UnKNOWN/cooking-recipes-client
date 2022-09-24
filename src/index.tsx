5f46bd1d0702f1dc4015be9304a4a060457f1482
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-09-24 19:09:30 +0800

    implemented route based navigation

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import './index.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import store from './store';
 import App from './App';
 
@@ -9,6 +10,8 @@ const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>
 );
