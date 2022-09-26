05710782db4e013a72fac990df86465e413e43f3
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-09-26 22:37:43 +0800

    increased font size for each recipe info

--- src/pages/Home/components/Recipes/components/Recipe/Recipe.tsx
+++ src/pages/Home/components/Recipes/components/Recipe/Recipe.tsx
@@ -5,15 +5,15 @@ import React from 'react';
 const Recipe = props => {
 
   return (
-    <div className='w-[250px] shadow-sm shadow-black mb-[15px]'>
+    <div className='w-[300px] shadow-sm shadow-black mb-[15px]'>
       <div className='border border-[#AC80F3]'>
         <video>
           <source src="" type="video/mp4" />
         </video>
       </div>
       <div className='p-[15px]'>
-        <p className='text-[13px] pb-2 text-center font-bold'>Recipe Title</p>
-        <p className='text-[9px] pb-2 text-center'>
+        <p className='text-[18px] pb-2 text-center font-bold'>Recipe Title</p>
+        <p className='text-[14px] pb-2 text-center'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
           molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
           numquam blanditiis harum quisquam
