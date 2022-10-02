f1b0e768588a54cb8cefefc2860162d57b2c64b2
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-10-02 22:25:36 +0800

    deleting a recipe will now reflects immediately

--- src/pages/Home/components/Recipes/components/Recipe/recipe.hook.tsx
+++ src/pages/Home/components/Recipes/components/Recipe/recipe.hook.tsx
@@ -1,11 +1,29 @@
+import { useDispatch, useSelector } from "react-redux";
+
+import { setRecipes } from "@reducers/recipes";
 import useRecipeServices from "./recipe.services";
+import type { Recipe } from "@reducers/recipes/recipe.types";
 
 const useRecipe = props => {
+  const dispatch = useDispatch();
+  const [
+    recipes,
+  ] = useSelector(state => [
+    state.recipes.data,
+  ]);
   const { deleteRecipe } = useRecipeServices();
 
   const deleteRecipeById = async (recipeId: number) => {
     const response = await deleteRecipe(recipeId);
-    response.data === 1 && console.log('deleted successfully')
+    response.data === 1 && removeRecipeFromRedux(recipeId);
+  }
+
+  const removeRecipeFromRedux = (recipeId: number) => {
+    dispatch(
+      setRecipes(
+        recipes?.filter((recipe: Recipe) => recipe.id !== recipeId)
+      )
+    );
   }
 
   return {
