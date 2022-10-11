4805007f85b29e648a12bb6e8c1bad9bf0e53b13
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-10-11 23:16:39 +0800

    modified global button component to have dynamic icon element

--- src/components/elements/Button/Button.tsx
+++ src/components/elements/Button/Button.tsx
@@ -11,16 +11,18 @@ const Button = props => {
   return (
     <button
       className={`bg-[#AC80F3] text-white w-[110px] h-[40px] text-[14px]
-        rounded-sm ${className ?? ""}
+        rounded-sm flex justify-center items-center ${className ?? ""}
       `}
       onClick={onClick}
     >
       {icon && (
-        <span className='mr-3'>
-          <i className={icon} aria-hidden="true"></i>
-        </span>
+        <div className='flex w-fit mr-2'>
+          {icon}
+        </div>
       )}
-      {children}
+      <div>
+        {children}
+      </div>
     </button>
   );
 }
