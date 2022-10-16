5dc744aaa799faaf5270f38f14658513c300fa73
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-10-16 19:01:56 +0800

    ability to add a debounce when typing in search filter

--- src/components/features/Search/Search.tsx
+++ src/components/features/Search/Search.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Input } from '@components/form/components';
 
 const Search = props => {
+  const {
+    onChange,
+  } = props;
+  const DEBOUNCE_TIMEOUT = 1000;
+  const [fireOnChange, setFireOnChange] = useState(true);
+  const [searchKey, setSearchKey] = useState('');
+
+  useEffect(() => {
+    // Debouce
+    setFireOnChange(true);
+    const timeOutId = setTimeout(() => {
+      fireOnChange && searchKey !== '' && onChange(searchKey);
+    }, DEBOUNCE_TIMEOUT);
+    return () => {
+      setFireOnChange(false);
+      clearTimeout(timeOutId);
+    }
+  }, [fireOnChange, searchKey]);
+
+  const changeHandler = e => {
+    setFireOnChange(false);
+    setSearchKey(e.target.value);
+  }
 
   return (
     <Input
       className="w-[280px] h-[40px]"
       name="search"
       placeholder="Search"
+      onChange={changeHandler}
     />
   );
 }
--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -4,12 +4,15 @@ import { useSelector } from 'react-redux';
 import Search from '@components/features/Search';
 
 const Header = props => {
+  const {
+    onChangeSearch,
+  } = props;
   const user = useSelector(state => state.user.data);
 
   return (
     <div className='flex justify-center items-center h-[50px] shadow-sm shadow-[rgba(0,0,0,.25)]'>
       <div className='hidden'></div>
-      <Search />
+      <Search onChange={onChangeSearch} />
       <div className='hidden'>{user.email || 'guest@email.com'}</div>
     </div>
   );
