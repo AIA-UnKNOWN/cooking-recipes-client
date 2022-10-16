2a9e1b4ce6ff60fa8d719066ac906381373e714d
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-10-16 20:13:46 +0800

    added profile icon besides the search field for profile page

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -10,8 +10,13 @@ const Header = props => {
   const user = useSelector(state => state.user.data);
 
   return (
-    <div className='flex justify-center items-center h-[50px] shadow-sm shadow-[rgba(0,0,0,.25)]'>
-      <div className='hidden'></div>
+    <div className='flex justify-evenly items-center h-[50px] shadow-sm shadow-[rgba(0,0,0,.25)]'>
+      <div className='h-[35px] w-[35px] flex justify-center items-center rounded-full hover:bg-[#E9E9E9]'>
+        <img
+          className="scale-75"
+          src="https://img.icons8.com/ios-glyphs/30/ac80f3/user.png"
+        />
+      </div>
       <Search onChange={onChangeSearch} />
       <div className='hidden'>{user.email || 'guest@email.com'}</div>
     </div>
