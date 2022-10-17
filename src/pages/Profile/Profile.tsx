4c2dc76aea6d8bfbfc0759adafa9b3e085832a90
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-10-17 23:42:17 +0800

    ability to logout from the app

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,18 +1,45 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import Cookies from 'js-cookie';
+import { useDispatch } from 'react-redux';
+import Swal from 'sweetalert2';
 
 import { Button } from '@components/elements';
 import {
   Input,
 } from '@components/form/components';
+import { setUser } from '@reducers/user';
 
 const Profile = props => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const [ user ] = useSelector(state => [state.user.data]);
 
   const goBack = () => {
     navigate('/');
+  }
+
+  const logout = async () => {
+    if (!Cookies.get('auth-token')) return;
+    const logoutConfirmationPopup = await Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    });
+    if (logoutConfirmationPopup.isConfirmed) {
+      Cookies.remove('auth-token');
+      dispatch(setUser({}));
+      Swal.fire(
+        'Logged Out!',
+        'You just logged out.',
+        'error'
+      );
+    }
   }
 
   return (
@@ -49,6 +76,13 @@ const Profile = props => {
             value={user?.username || 'anonymous'}
           />
         </div>
+      </div>
+      <div className='mb-[22px] flex justify-center'>
+        <Button
+          onClick={logout}
+        >
+          Logout
+        </Button>
       </div>
     </div>
   )
